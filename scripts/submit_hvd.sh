91afbc16fa90e34d9f0dd2a4a00c2bfa17d7c74b
ViniciusMikuni <vinicius.mikuni@gmail.com>
2021-11-17 14:30:15 -0800

    update submission script

--- scripts/submit_hvd.sh
+++ scripts/submit_hvd.sh
@@ -17,5 +17,5 @@ export MPICH_ALLGATHERV_PIPELINE_MSG_SIZE=0
 export MPICH_MAX_THREAD_SAFETY=multiple
 export MPIR_CVAR_GPU_EAGER_DEVICE_MEM=0
 
-cd $HOME/H1
+cd $HOME/H1/scripts
 srun python Unfold_offline.py --closure --niter 50 --pct 
