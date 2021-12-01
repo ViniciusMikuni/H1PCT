5c6644bb2632b9607ec68fc69c452d87d80b2948
ViniciusMikuni <vinicius.mikuni@gmail.com>
2021-11-30 17:12:13 -0800

    update general plotting scripts

--- scripts/Plot_gen.py
+++ scripts/Plot_gen.py
@@ -1,5 +1,4 @@
-#Plot the comparison between data and simulated events
-
+#Plot the comparison between MC reco and MC gen
 
 import time
 import numpy as np
@@ -25,25 +24,20 @@ opt.SetStyle()
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--data_folder', default='/clusterfs/ml4hep/vmikuni/H1/jet_subs/h5', help='Folder containing data and MC files')
-parser.add_argument('-N', type=int,default=5e6, help='Number of events to evaluate')
+parser.add_argument('-N', type=float,default=30e6, help='Number of events to evaluate')
 
 
 flags = parser.parse_args()
 flags.N = int(flags.N)
 
-mc_names = ['Rapgap','Djangoh']
-mc_tags = ['nominal','nominal']
+mc_names = ['Rapgap']
+mc_tags = ['nominal']
     
 
-data_name = 'data'
-data_vars = h5.File(os.path.join(flags.data_folder,"{}.h5".format(data_name)),'r')
-
-
-
 var_names = {
-    'Q2': r"$Q^2$",
-    'jet_pt': r"$p_\mathrm{T}^\mathrm{jet}$",
-    'jet_eta':r'$\eta^\mathrm{jet}$',
+    #'Q2': r"$Q^2$",
+    # 'jet_pt': r"$p_\mathrm{T}^\mathrm{jet}$",
+    # 'jet_eta':r'$\eta^\mathrm{jet}$',
 
 
     # 'genjet_phi':r'$\phi^\mathrm{jet}$',
@@ -65,9 +59,10 @@ var_names = {
 class MCInfo():
     def __init__(self,mc,tag,N,data_folder):
         self.N = N
-        self.predictions = h5.File(os.path.join(data_folder,"{}_{}.h5".formt(mc,tag)),'r')
+        self.predictions = h5.File(os.path.join(data_folder,"{}_{}.h5".format(mc,tag)),'r')
         self.nominal_wgts = self.predictions['wgt'][:self.N]
-        self.fiducial_masks = self.predictions['pass_reco'][:self.N] #pass fiducial region definition
+        self.reco_masks = self.predictions['pass_reco'][:self.N] #pass fiducial region definition
+        self.fiducial_masks = self.predictions['pass_fiducial'][:self.N] #pass fiducial region definition
 
 mc_info = {}
 
@@ -78,35 +73,36 @@ for mc,tag in zip(mc_names,mc_tags):
 
 for var in var_names:
     print(var)
-    binning = opt.dedicated_binning[var]    
-
-    data_var = data_vars[var][:]
-
-    if 'tau' in var:
-        data_var = np.log(data_var)    
-
+    binning = opt.dedicated_binning["gen_"+var]
     fig,gs = opt.SetGrid() 
     ax0 = plt.subplot(gs[0])
     
-
-    data_pred,_,_=ax0.hist(data_var,bins=binning,label="Data",density=True,color="black",histtype="step")
-    print(var,data_pred)
-
-    opt.FormatFig(xlabel = "", ylabel = 'Normalized Events / bin',ax0=ax0)
-    if var == 'jet_pt':
-        plt.yscale("log")  
-        plt.xscale("log")  
+        
     ratios = {}
+    
     for mc in mc_names:
-        mask = mc_info[mc].fiducial_masks==1
-        mc_var = mc_info[mc].predictions[var][:flags.N][mask]
+        mask_reco = mc_info[mc].reco_masks==1
+        mask_fid = mc_info[mc].fiducial_masks==1
+        mc_reco = mc_info[mc].predictions[var][:flags.N][mask_reco]
+        mc_gen = mc_info[mc].predictions["gen_"+var][:flags.N][mask_fid]
+        
         if 'tau' in var:
-            mc_var = np.log(mc_var)    
+            mc_reco = np.log(mc_reco)
+            mc_gen = np.log(mc_gen)            
         
-        pred,_,_=ax0.hist(mc_var,weights=mc_info[mc].nominal_wgts[mask],bins=binning,label=mc,density=True,color=opt.colors[mc],histtype="step")
-        ratios[mc] = 100*np.divide(pred-data_pred,data_pred)
+        pred_reco,_,_=ax0.hist(mc_reco,weights=mc_info[mc].nominal_wgts[mask_reco],bins=binning,label=mc+" Reco.",density=True,color=opt.colors[mc],histtype="step")
+        pred_gen,_,_=ax0.hist(mc_gen,weights=mc_info[mc].nominal_wgts[mask_fid],bins=binning,label=mc+" Gen.",density=True,color="black",histtype="step")
+        opt.FormatFig(xlabel = "", ylabel = 'Normalized Events / bin',ax0=ax0)
         
-    ax0.legend(loc='lower right',fontsize=16,ncol=1)
+        if 'jet_pt' in var and 'D' not in var:
+            plt.yscale("log")  
+            plt.xscale("log")
+
+        
+        
+        ratios[mc] = 100*np.divide(pred_gen-pred_reco,pred_gen)
+        
+    ax0.legend(loc='best',fontsize=16,ncol=1)
     plt.xticks(fontsize=0)
 
 
@@ -125,7 +121,7 @@ for var in var_names:
 
     plt.ylim([-20,20])
     
-    plot_folder = '../plots_data'
+    plot_folder = '../plots_gen'
     if not os.path.exists(plot_folder):
         os.makedirs(plot_folder)
-    fig.savefig(os.path.join(plot_folder,"{}.pdf".format(var)))
+    fig.savefig(os.path.join(plot_folder,"{}_gen.pdf".format(var)))
